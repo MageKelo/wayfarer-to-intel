c8cdc64943bc1d6223f24bf3beda37d003a043e3
志寒 <wzhkelo@gmail.com>
2024-10-11 19:03:43 +0800

    Update search-wayspot.js
    1.0 edit

--- search-wayspot.js
+++ search-wayspot.js
@@ -1,41 +1,42 @@
 // ==UserScript==
-// @name         search-wayspot
-// @namespace    http://tampermonkey.net/
-// @version      2024-10-07
-// @description  try to take over the world!
-// @author       Kelo
-// @match        https://intel.ingress.com/intel?ll=*.*&z=18&pll=*,*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=ingress.com
-// @grant        none
+// @name       search-wayspot
+// @namespace   http://tampermonkey.net/
+// @match           https://intel.ingress.com/intel?ll=*.*&z=18&pll=*,*
+// @author kelo
+// @updateURL  https://github.com/MageKelo/wayfarer-to-intel/blob/main/search-wayspot.js
+// @downloadURL  https://github.com/MageKelo/wayfarer-to-intel/blob/main/search-wayspot.js
+// @version     1.0
+// @grant       none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // 获取 URL 参数
-    const params = new URLSearchParams(window.location.search);
-    const ll = params.get('ll'); // 获取 ll 参数
-    const latLng = ll ? ll.split(',') : []; // 将经纬度分割为数组
-    const lat = latLng[0]; // 纬度
-    const lng = latLng[1]; // 经度
-    // 查找搜索框
-    const addressInput = document.querySelector('#search');
-
-    if (addressInput) {
-        // 设置默认值为 "${lat},${lng}"
-        addressInput.value = `${lat},${lng}`;
-
-        // 触发输入事件
-        const inputEvent = new Event('input', {
-            bubbles: true,
-            cancelable: true
-        });
-        addressInput.dispatchEvent(inputEvent); // 触发输入事件
-
-        // 查找表单并提交
-        const form = document.querySelector('#geocode');
-        if (form) {
-            form.dispatchEvent(new Event('submit', { bubbles: true })); // 触发表单提交
-        }
+    // 确保地图已加载
+    if (!window.map || !L.marker) {
+        alert('地图或Leaflet标记功能未加载！');
+        return;
     }
+
+    // 从URL中获取经纬度参数
+    const params = new URLSearchParams(window.location.search);
+    const ll = params.get('ll'); // 获取 'll' 参数，包含纬度和经度
+    const latLng = ll ? ll.split(',') : [];
+    const lat = parseFloat(latLng[0]); // 纬度
+    const lng = parseFloat(latLng[1]); // 经度
+
+    if (lat && lng) {
+       const poratl = new Object({
+           lat: lat,
+           lng: lng
+       });
+        console.log(poratl);
+        var c = L.circle( poratl, 20, '{ fill: "#F423", color: "#F42", weight: 1, clickable: false }' ).addTo(map);
+
+        var m = L.marker( poratl ,{ draggable: false, icon: new L.Icon.Default() }).addTo(map).on('dragend', function(e){
+            var coords = e.target.getLatLng();
+            c.setLatLng( coords );
+        });
+    }
+   
 })();
